8410d3295e15a746cddc1edcec8fd66378c22fd6
Abhishek Narwariya <abhisheknarwariya813@gmail.com>
2022-05-19 14:19:57 +0530

    Append items

--- DataStructure/Program.cs
+++ DataStructure/Program.cs
@@ -8,10 +8,16 @@ namespace LinkedList
         static void Main(string[] args)
         {
             LinkedList list = new LinkedList();
-            list.Add(56);
-            list.Add(30);
-            list.Add(70);
 
+            //list.Add(56);
+            //list.Add(30);
+            //list.Add(70);
+
+            //list.Display();
+
+            list.Add(56);
+            list.Append(30);
+            list.Append(70);
             list.Display();
 
 
