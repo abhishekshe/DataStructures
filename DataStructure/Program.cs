f6c6c9690db016c4bda9ce51f58d8d91d8e88001
Abhishek Narwariya <abhisheknarwariya813@gmail.com>
2022-05-19 14:44:25 +0530

    Adding Inbetween

--- DataStructure/Program.cs
+++ DataStructure/Program.cs
@@ -9,16 +9,32 @@ namespace LinkedList
         {
             LinkedList list = new LinkedList();
 
+            // (****UC1****)
             //list.Add(56);
             //list.Add(30);
             //list.Add(70);
 
             //list.Display();
 
+
+            // (*****UC3****)
+            //list.Add(56);
+            //list.Append(30);
+            //list.Append(70);
+            //list.Display();
+
+
+            // (***UC4***)
+
             list.Add(56);
-            list.Append(30);
-            list.Append(70);
+            
+            list.Add(70);
+            int a = list.search(56);
+            list.InBetween(a + 2, 30);
+            Console.WriteLine("index position of element 56 is :" + a);
+           
             list.Display();
+
 
 
 
